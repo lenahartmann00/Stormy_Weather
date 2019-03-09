861af041d3acab314c8c4d1f7a77c900dc811714
Lena Hartmann <lena.hartmann00@googlemail.com>
2019-03-09 14:26:04 +0100

    added class currentlocation

--- app/src/main/java/com/lenahartmann00/stormyweather/model/CurrentLocation.java
+++ app/src/main/java/com/lenahartmann00/stormyweather/model/CurrentLocation.java
@@ -1,10 +1,14 @@
 package com.lenahartmann00.stormyweather.model;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import com.lenahartmann00.stormyweather.R;
+import com.lenahartmann00.stormyweather.ui.MainActivity;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,6 +21,15 @@ public class CurrentLocation {
     private  double longitude;
     private String city;
 
+
+    private void getCurrentLocationData() {
+        if (ActivityCompat.checkSelfPermission(, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        } else{
+            updateCurrentLocation();
+        }
+    }
 
     //Getter and Setter
     public double getLatitude() {
